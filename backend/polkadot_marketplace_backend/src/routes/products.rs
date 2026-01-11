169f8f37ccbfd7494089e52cdcf013caa1dcec07
Clyn1 <ochiengclyntono@gmail.com>
2026-01-11 23:02:34 +0300

    fixed api 403 error issue

--- backend/polkadot_marketplace_backend/src/routes/products.rs
+++ backend/polkadot_marketplace_backend/src/routes/products.rs
@@ -100,3 +100,4 @@ pub async fn search_products(
         Err(err) => Err(err),
     }
 }
+                                                                                                                                                                                                                                    
